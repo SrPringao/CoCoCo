778552676d2bf732f0179519c1eee18652ad2d0a
FranqZD <a19300095@ceti.com.mx>
2023-05-07 13:59:46 -0600

    Insert Godin

--- src/cococo/Models/Usuario.java
+++ src/cococo/Models/Usuario.java
@@ -9,5 +9,24 @@ package cococo.Models;
  * @author franc
  */
 public class Usuario {
+        public int id;
+    public String nombre;
+    public String Password;
+    public int Estatus;
     
+    public Usuario()
+    {
+        id = 0;
+        nombre = "";
+        Password = "";
+        Estatus = 0;
+    }
+
+    public Usuario(String nombre, String Password, int Estatus) 
+    {
+        this.id = id;
+        this.nombre = nombre;
+        this.Password = Password;
+        this.Estatus = 0;
+    }
 }
