b4c158e976e0881ce0c3067f04082bd327b51a45
eE06 <hector8heredia@gmail.com>
2023-05-08 21:02:48 -0600

    Notas

--- Client/src/client/Client.java
+++ Client/src/client/Client.java
@@ -1,42 +1,59 @@
 package client;
 
 import java.net.Socket;
+import java.net.Inet4Address;
+import java.net.ServerSocket;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Scanner;
 
 public class Client {
-    private static Socket clientSocket;
-    
+    private static Socket serverSocket;
+    private static Socket mySocket;
+    private static Socket z;
     
     
     public static void main(String[] args) {
         try {
-            clientSocket = new Socket("192.168.84.62", 1458);
+            serverSocket = new Socket("192.168.84.62", 1458);
+
+            mySocket.getLocalAddress();
+            mySocket.getLocalPort();
+            
         } catch (Exception e) {
             System.out.println("Error de conexión");
         }
         
         Client correr = new Client();
-        
-            Scanner scanner = new Scanner(System.in);
-    
-            System.out.println("Ingresa tu mensaje: ");
+        // correr.run(myip); // Para enviar la ip actual del cliente al servidor y que allá el serv la actualice con un update a mi id, quizá necesite separar myip de mi id para ponerlo donde va
             correr.run("Héctor se ha unido a la conversación");
-            boolean session = true;
-        while (session == true) // cambiar por (session == true) y que cierre la conexión en cuanto se cierre sesión
-        {
-            String mensaje = scanner.nextLine();
-            mensaje = "Hector" + ": " + mensaje; // CAMBIAR "Hector" POR SELECT GLOBAL DEL USUARIO
+            boolean session = true; // cambiar por (session == true) y que cierre la conexión en cuanto se cierre sesión
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Ingresa tu mensaje: ");
             
+
+        while (session == true) 
+        {
+            String user = "Hector"; // CAMBIAR "Hector" POR SELECT GLOBAL DEL USUARIO
+            // Envío
+            String mensaje = scanner.nextLine();
+            mensaje = user + ": " + mensaje; 
             correr.run(mensaje);
+            
+            // Recepción
+            
+            BufferedReader in = new BufferedReader(new InputStreamReader(z.getInputStream()));
+            String input = in.readLine();
+            System.out.println("Mensaje recibido: " + input);
         }
         scanner.close();
     }
     
     public void run(String mensaje) {
         try {
-            OutputStreamWriter out = new OutputStreamWriter(clientSocket.getOutputStream());
+            OutputStreamWriter out = new OutputStreamWriter(serverSocket.getOutputStream());
             out.write(mensaje + "\n");
             out.flush();
         } catch (IOException e) {
