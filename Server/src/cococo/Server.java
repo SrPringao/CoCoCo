810a5e4f2e8ffb84c4d42a0812aea355f7dfc82d
DonitaCosmica <a18300356@ceti.mx>
2023-05-11 19:05:03 -0500

    Server Funciona

--- Server/src/cococo/Server.java
+++ Server/src/cococo/Server.java
@@ -6,94 +6,39 @@ import Models.*;
 import java.io.BufferedReader;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 
 public class Server {
-    private Socket c;
-    private ArrayList<Socket> clients = new ArrayList<>();
-    private ServerSocket serverSocket;
+    public static Map<Integer, Socket> usuariosConectados = new ConcurrentHashMap<>();
 
-    public Server(int port) {
-        try {
-            serverSocket = new ServerSocket(port);
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(1234);
+        System.out.println("Servidor iniciado, esperando conexiones...");
 
-        } catch (IOException e) {
-            e.printStackTrace();
+        while (!serverSocket.isClosed()) {
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("Conexión entrante desde " + clientSocket.getInetAddress().getHostAddress());
+            Thread t = new Thread(new ManejadorCliente(clientSocket));
+            t.start();
+            System.out.println("Id del Hilo: " + t.getId());
+            usuariosConectados.put(clientSocket.hashCode(), clientSocket);
+
+
+            System.out.println(usuariosConectados.get(clientSocket.hashCode()));
         }
     }
 
-    public void start() {
-        Chat chat;
-        UsuarioDAO usuarioDAO = new UsuarioDAO();
-
-        Usuario usr = new Usuario();
-
-        while (true) {
-            try {
-                // aquí se espera a que un cliente se conecte
-                System.out.println("Esperando...");
-                // aquí se acepta la conexión
-                c = serverSocket.accept();
-                // aqui se agrega el socket a la lista de sockets
-                clients.add(c);
-
-                boolean loggedIn = false;
-                while (!loggedIn) {
-                    BufferedReader in = new BufferedReader(new InputStreamReader(c.getInputStream()));
-                    String input = in.readLine();
-
-                    String[] parts = input.split("\\|");
-
-                    // Aquí se llama al método login()
-                    loggedIn = login(parts[0], parts[1]);
-
-                    if (!loggedIn) {
-                        // Si el usuario no está autenticado, cierra la conexión del cliente
-                        c.close();
-                        clients.remove(c);
-                    }
-                }
-
-                System.out.println(c.getInputStream().read());
-                BufferedReader in = new BufferedReader(new InputStreamReader(c.getInputStream()));
-                String input = in.readLine();
-
-                String[] parts = input.split("\\|");
-
-                chat = new Chat(Integer.parseInt(parts[0]), Integer.parseInt(parts[1]), parts[2]);
-                usuarioDAO.addChat(chat);
-
-                System.out.println("Mensaje recibido: " + input);
-                String mensaje = input;
-
-
-                // Socket s = new Socket("192.168.1.70", 1234); // PONER AQUÍ EL REMITENTE
-                // byte[] bytes = mensaje.getBytes();
-                // s.getOutputStream().write(bytes);
-
-                for (Socket s : clients) {
-                    if (s != c) { // Evitar reenviar el mensaje al cliente que lo envió
-                        OutputStreamWriter out = new OutputStreamWriter(s.getOutputStream());
-                        out.write(mensaje + "\n");
-                        out.flush();
-                    }
-                }
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    private boolean login(String username, String password) {
-        UsuarioDAO usuarioDAO = new UsuarioDAO();
-        if (!usuarioDAO.verificarUsuario(username, password)) {
-            return false;
-        } else {
-            return true;
-        }
+    public static void sendMessage(String mensaje,Socket sender) throws IOException
+    {
+        System.out.println(mensaje);
+        mensaje="Ayudame Dios";
+        PrintWriter out=new PrintWriter(sender.getOutputStream(),false);
+    out.write(mensaje+"\n");
+        out.flush();
     }
 }
